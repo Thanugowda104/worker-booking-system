edc55ea4ecb1e6d00a102fa3fa2fb64fd7438084
Thanushree <thanushrivasanth@gmail.com>
2026-06-30 21:20:21 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -8,11 +8,13 @@ $port = "5432";
 
 try {
     $pdo = new PDO(
-        "pgsql:host=$host;port=$port;dbname=$db",
+        "pgsql:host=$host;port=$port;dbname=$db;sslmode=require",
         $user,
         $pass
     );
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
