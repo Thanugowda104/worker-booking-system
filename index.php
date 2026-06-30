1a5d2f45c6bae9ee8ff82259d809ab0cd05a985f
Thanushree <thanushrivasanth@gmail.com>
2026-06-30 21:34:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-// index.php
+require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/config/database.php';
-require_once __DIR__ . '/includes/functions.php';
+
 header("Location: " . BASE_URL . "auth/login.php");
 exit;
